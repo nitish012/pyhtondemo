06507449aa30f874cfdca0582443ca035f664dc0
nitish012 <nitishdewan@qainfotech.com>
2019-08-08 19:43:03 +0530

    second commit

--- startdemo/demo/quickstart/urls.py
+++ startdemo/demo/quickstart/urls.py
@@ -1,8 +1,22 @@
 from django.conf.urls import url
-from rest_framework import routers,DefaultRouter
+from django.urls import path
+# from rest_framework import routers
 from quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'students', views.StudentViewSet,'students')
+# router = routers.DefaultRouter()
+# router.register(r'students', views.StudentViewSet)
 
-urlpatterns = router.urls
+# urlpatterns = [
+#     path('', include(router.urls))
+# ]
+
+
+urlpatterns = [
+    path('students/',views.StudentviewSet.as_view({'get':'list'})),
+    path('student/<int:pk>/',views.StudentviewSet.as_view({'get':'retrieve'})),
+    path('student/update/<int:pk>/',views.StudentviewSet.as_view({'post':'create'})),
+
+    path('student/insert/',views.StudentInsert.as_view()),
+    path('student/update/<int:pk>/',views.StudentList.as_view()),
+    # path('student/',views.StudentviewSet.as_view({'get':'create'})),
+]
--- startdemo/demo/quickstart/views.py
+++ startdemo/demo/quickstart/views.py
@@ -1,30 +1,50 @@
-from rest_framework import generics,viewsets
+from rest_framework import viewsets,generics
+from quickstart.models import Student
+from django.shortcuts import get_object_or_404
 from quickstart.serializers import StudentSerializer
-from quickstart.models import Student, Teacher
-from rest_framework import mixins
+from rest_framework.response import Response
 
-
-# Create your views here.
+#Create your views here.
 
 # class StudentViewSet(viewsets.ModelViewSet):
 #     queryset = Student.objects.all()
 #     serializer_class = StudentSerializer
 
-
-class StudentviewSet(mixins.CreateModelMixin,
-                                mixins.ListModelMixin,
-                                mixins.RetrieveModelMixin,
-                                viewsets.GenericViewSet):
+class StudentviewSet(viewsets.ViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    # def list(self, request):
-    #     queryset = Student.objects.all()
-    #     serializer = StudentSerializer(queryset, many=True)
-    #     return Response(serializer.data)
+    def list(self, request):
+        queryset = Student.objects.all()
+        serializer = StudentSerializer(queryset, many=True)
+        return Response(serializer.data)
     
-    # def retrieve(self, request, pk=None):
-    #     queryset = Student.objects.all()
-    #     user = get_object_or_404(queryset, pk=pk)
-    #     serializer = StudentSerializer(user)
-    #     return Response(serializer.data)
+    def retrieve(self, request, pk=None):
+        queryset = Student.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = StudentSerializer(user)
+        return Response(serializer.data)
+
+    # def create(self,request):
+    #     serializer = StudentSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        if request.data.get('id'):
+            return super(StudentViewSet, self).update(request, *args, **kwargs)
+        else:
+            return super(StudentViewSet, self).create(request, *args, **kwargs)
+
+class StudentInsert(generics.CreateAPIView):
+    ''' Inserting a records of students'''
+    queryset = Student.objects.all()
+    serializer_class=StudentSerializer
+
+
+class StudentList(generics.RetrieveUpdateDestroyAPIView):
+    ''' fetching list of students'''
+    queryset = Student.objects.get_queryset()
+    serializer_class=StudentSerializer    
