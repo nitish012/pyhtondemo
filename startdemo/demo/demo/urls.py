f30f85ff2bc1ae4a101f3e3da8119abd6123d8c5
geekyhubster <40542582+nitish012@users.noreply.github.com>
2019-08-16 18:23:45 +0530

    Api work (#1)
    * second commit
    
    * start demo commit

--- startdemo/demo/demo/urls.py
+++ startdemo/demo/demo/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from rest_framework import routers
+from quickstart import views
 
+# router = routers.DefaultRouter()
+# router.register(r'students', views.StudentViewSet)
 
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('admin/', admin.site.urls),
+# ]
 urlpatterns = [
-    path('', include('quickstart.urls')),
+    path('quickstart/',include('quickstart.urls')), 
     path('admin/', admin.site.urls),
-]
 
+]
--- startdemo/demo/quickstart/urls.py
+++ startdemo/demo/quickstart/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-from rest_framework import routers,DefaultRouter
+from django.urls import path
+# from rest_framework import routers
 from quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'students', views.StudentViewSet,'students')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('students/',views.StudentviewSet.as_view({'get':'list','post':'create'})),
+    path('students/<int:pk>/',views.StudentviewSet.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
+    path('teachers/',views.TeacherviewSet.as_view({'get':'list','post':'create'})),
+    path('teachers/<int:pk>/',views.TeacherviewSet.as_view({'get':'retrieve','put':'update','delete':'destroy'})),
+     
+]
